985f56a413ab8956828dba8beb216da62700e79d
Charl Broekman <charlbroekman11@gmail.com>
2024-03-13 14:19:34 +0200

    product interface nou in interfaces folder

--- src/app/Modules/Products/Services/product.service.ts
+++ src/app/Modules/Products/Services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { IApiResponse, IProduct } from '../../../../Products_interface';
+import { IApiResponse, IProduct } from '../Interfaces/Products_interface';
 import { AuthService } from '../../Users/Services/auth.service';
 
 @Injectable({
